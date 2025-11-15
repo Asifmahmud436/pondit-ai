1bbcb3228776c32655f5ca9ab597b54252bf792e
Asifmahmud436 <safaandsafa4.com>
2025-11-15 12:17:46 +0600

    can run gemini from cli now

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("No prompt was provided")
+        exit(1)
+    contents = sys.argv[1]
+    messages = [
+        types.Content(role='user',parts=[types.Part(text=contents)])
+    ]
+    
+    if '--verbose' in sys.argv:
+        response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=contents,
+        )
+        print("User prompt:",contents)
+        print("Prompt tokens:",response.usage_metadata.prompt_token_count)
+        print("Response tokens:",response.usage_metadata.candidates_token_count)
+    else:
+        response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        )
+        print(response.text)
+        
+
+
+if __name__ == "__main__":
+    main()
